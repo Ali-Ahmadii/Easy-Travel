bed1204e7f46a078c888841233aaed8686d1f1f6
Parsa <ParyPary82@gmail.com>
2022-06-14 18:25:54 +0430

    test

--- EasyTravelPro/src/main/java/com/example/easytravelpro/Main.java
+++ EasyTravelPro/src/main/java/com/example/easytravelpro/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("ScreenLoader.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignInUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         Image image = new Image("hostel.png");
--- EasyTravelPro/src/main/java/com/example/easytravelpro/SignInUp.java
+++ EasyTravelPro/src/main/java/com/example/easytravelpro/SignInUp.java
@@ -8,38 +8,52 @@ import javafx.scene.layout.VBox;
 public class SignInUp {
 
     @FXML
-    private ImageView beachPicture;
+    private ImageView beachPicture, roomPicture;
     @FXML
-    private VBox signUpPage;
+    private VBox signUpPage, signInPage;
     @FXML
-    private Label suBtn,alreadyLogged;
+    private Label suBtn,alreadyLogged,siBtn,newUser;
 
     public void changeColorEntered() {
         suBtn.setStyle("-fx-background-color: #ffaf52;" +
+                "-fx-background-radius: 50;");
+
+        siBtn.setStyle("-fx-background-color: #ffaf52;" +
                 "-fx-background-radius: 50;");
     }
 
     public void changeColorExited() {
         suBtn.setStyle("-fx-background-color: #FF9212;" +
                 "-fx-background-radius: 50;");
+
+        siBtn.setStyle("-fx-background-color: #FF9212;" +
+                "-fx-background-radius: 50;");
     }
 
     public void changeColorPressed() {
         suBtn.setStyle("-fx-background-color: #e38600;" +
+                "-fx-background-radius: 50;");
+
+        siBtn.setStyle("-fx-background-color: #e38600;" +
                 "-fx-background-radius: 50;");
     }
 
     public void changeColorReleased() {
         suBtn.setStyle("-fx-background-color: #ffaf52;" +
                 "-fx-background-radius: 50;");
+
+        siBtn.setStyle("-fx-background-color: #ffaf52;" +
+                "-fx-background-radius: 50;");
     }
 
     public void changeColorEnter() {
         alreadyLogged.setStyle("-fx-text-fill: #3172f6;");
+        newUser.setStyle("-fx-text-fill: #3172f6;");
     }
 
     public void changeColorExit() {
         alreadyLogged.setStyle("-fx-text-fill: #123d93;");
+        newUser.setStyle("-fx-text-fill: #123d93;");
     }
 
 }
